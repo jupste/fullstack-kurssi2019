b90e7409ea7a1318c71c38f697733f9b35a5f051
Jussi Steenari <jussi.steenari@helsinki.fi>
2019-02-08 16:15:03 +0200

    toimii

--- part4/blogit/index.js
+++ part4/blogit/index.js
@@ -1,29 +1,9 @@
-require('dotenv').config()
+const config = require('./utils/config')
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const Blog = require('./models/blog')
 
-app.use(bodyParser.json())
-app.use(cors())
+const server = http.createServer(app)
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then(result => {
-    response.status(201).json(result)
-  })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
